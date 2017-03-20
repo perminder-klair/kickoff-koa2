60849373695039315e97efc977552e5561021ce8
Parminder Klair <perminder.klair@gmail.com>
2017-03-20 20:31:28 +0000

    added some styles to landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -7,8 +7,28 @@ export default () => (
             <title>Koa2 React app</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
+        <style jsx>{`
+            * {
+                margin: 0;
+                padding: 0;
+                box-sixing: border-box;
+            }
+            html, body {
+                font-family: 'Helvetica Neue', 'Helevetica', 'Arial', 'sans-serif';
+                font-weight: 400;
+                font-size: 16px;
+                color: #333;
+            }
+            .container {
+                margin-top: 100px;
+                padding: 20px;
+            }
+            .center-text {
+                text-align: center;
+            }
+        `}</style>
         <div className="container">
-            <h1>Welcome to Koajs + Reactjs app!</h1>
+            <h1 className="center-text">Welcome to Koa.js + React.js app</h1>
         </div>
     </div>
 );
