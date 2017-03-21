74f0a0e2b3c11d9fa3f3894c01587e71084342eb
Parminder Klair <p.klair@scorchsoft.com>
2017-03-21 13:44:17 +0000

    updated graphql schema

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,19 +1,21 @@
-const resolvers = (models) => ({
-  Query: {
-    getUserById(root, { id }) {
-      return models.User.findById(id).then((response) => response);
+const resolvers = models => ({
+    Query: {
+        users() {
+            return models.User.find().then(response => response);
+        },
+        getUserById(root, { id }) {
+            return models.User.findById(id).then(response => response);
+        },
+        getUserByEmail(root, { email }) {
+            return models.User.findOne({ email }).then(response => response);
+        },
     },
-
-    getUserByEmail(root, { email }) {
-      return models.User.findOne({ email }).then((response) => response);
+    Mutation: {
+        createUser(root, args) {
+            const user = new models.User(args);
+            return user.save().then(response => response);
+        },
     },
-  },
-  Mutation: {
-    createUser(root, args) {
-      const user = new models.User(args);
-      return user.save().then((response) => response);
-    },
-  },
 });
 
 module.exports = resolvers;
